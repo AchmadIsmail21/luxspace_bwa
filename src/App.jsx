01aa0abc62b38064770a71705356a9fe11be6366
unknown <achmadismail423@gmail.com>
2023-02-13 20:19:06 +0700

    update folder structuring

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'assets/css/app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text text-xl'>
+        Hero
+      </h1>
+    </div>
+  );
+}
+
+export default App;
